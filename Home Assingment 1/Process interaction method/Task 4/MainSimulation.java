82210609fe5b45830abde488caff5bf87f3e31e2
Stronkness <andrefrisk98@gmail.com>
2023-04-26 14:02:51 +0200

    Lab2 + Correcttion again H1

--- Home Assingment 1/Process interaction method/Task 4/MainSimulation.java
+++ Home Assingment 1/Process interaction method/Task 4/MainSimulation.java
@@ -7,51 +7,52 @@ import java.io.*;
 public class MainSimulation extends Global {
 
 	public static void main(String[] args) throws IOException {
-
+		int[] iterations = { 20, 50, 100, 1000 };
 		// Signallistan startas och actSignal deklareras. actSignal r den senast
 		// utplockade signalen i huvudloopen nedan.
 		// The signal list is started and actSignal is declaree. actSignal is the latest
 		// signal that has been fetched from the
 		// signal list in the main loop below.
+		for (int iteration : iterations) {
+			Signal actSignal;
+			new SignalList();
 
-		Signal actSignal;
-		new SignalList();
+			// H r nedan skapas de processinstanser som beh vs och parametrar i dem ges v
+			// rden.
+			// Here process instances are created (two queues and one generator) and their
+			// parameters are given values.
 
-		// H r nedan skapas de processinstanser som beh vs och parametrar i dem ges v
-		// rden.
-		// Here process instances are created (two queues and one generator) and their
-		// parameters are given values.
+			QS Q1 = new QS();
+			Q1.sendTo = null;
 
-		QS Q1 = new QS();
-		Q1.sendTo = null;
+			Q1.lambda = 1.0 / 4;
 
-		Q1.lambda = 1.0 / 4;
+			Gen Generator = new Gen();
+			Generator.lambda = 1.0 / 5; // Generator ska generera nio kunder per sekund //Generator shall generate 9
+										// customers per second
+			Generator.sendTo = Q1; // De genererade kunderna ska skickas till k systemet QS // The generated
+									// customers shall be sent to Q1
 
-		Gen Generator = new Gen();
-		Generator.lambda = 1.0 / 5; // Generator ska generera nio kunder per sekund //Generator shall generate 9
-									// customers per second
-		Generator.sendTo = Q1; // De genererade kunderna ska skickas till k systemet QS // The generated
-								// customers shall be sent to Q1
+			// H r nedan skickas de f rsta signalerna f r att simuleringen ska komma ig ng.
+			// To start the simulation the first signals are put in the signal list
 
-		// H r nedan skickas de f rsta signalerna f r att simuleringen ska komma ig ng.
-		// To start the simulation the first signals are put in the signal list
+			SignalList.SendSignal(READY, Generator, time);
+			SignalList.SendSignal(MEASURE, Q1, time);
 
-		SignalList.SendSignal(READY, Generator, time);
-		SignalList.SendSignal(MEASURE, Q1, time);
+			// Detta r simuleringsloopen:
+			// This is the main loop
 
-		// Detta r simuleringsloopen:
-		// This is the main loop
+			while (Q1.numberOfArrivals < iteration) {
+				actSignal = SignalList.FetchSignal();
+				time = actSignal.arrivalTime;
+				actSignal.destination.TreatSignal(actSignal);
+			}
 
-		while (Q1.numberOfArrivals < 1000) {
-			actSignal = SignalList.FetchSignal();
-			time = actSignal.arrivalTime;
-			actSignal.destination.TreatSignal(actSignal);
+			// Slutligen skrivs resultatet av simuleringen ut nedan:
+			// Finally the result of the simulation is printed below:
+			System.out.println(iteration);
+			System.out.println("average queuing time for Special: " + Q1.specialQueueTime / Q1.specialDone);
+			System.out.println("average queuing time for Normal: " + Q1.normalQueueTime / Q1.normalDone + "\n");
 		}
-
-		// Slutligen skrivs resultatet av simuleringen ut nedan:
-		// Finally the result of the simulation is printed below:
-
-		System.out.println("average queuing time for Special: " + Q1.specialQueueTime / Q1.specialDone);
-		System.out.println("average queuing time for Normal: " + Q1.normalQueueTime / Q1.normalDone);
 	}
 }
---
+++ Lab 2/Task 2/IP_1-solution-A.m
@@ -0,0 +1,31 @@
+clc
+clear all
+
+f = [1 1 1 1 1 1 1];
+A = [-1 0 0 -1 -1 -1 -1;
+     -1 -1 0 0 -1 -1 -1;
+     -1 -1 -1 0 0 -1 -1;
+     -1 -1 -1 -1 0 0 -1;
+     -1 -1 -1 -1 -1 0 0;
+     0 -1 -1 -1 -1 -1 0;
+     0 0 -1 -1 -1 -1 -1;
+     ];
+ b = [-8;
+     -6;
+     -5;
+     -4;
+     -6;
+     -7;
+     -9;
+      ];
+ lb = zeros(7,1);
+ %options = optimoptions('intlinprog', 'Display', 'iter');
+ options = optimoptions('intlinprog', 'Display', 'off');
+ intcon = [1;
+           2;
+           3;
+           4;
+           5;
+           6;
+           7;];
+[x, fval, exitflag, output] = intlinprog(f', intcon, A, b, [], [], lb, [], options)
